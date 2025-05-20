69993e135dd41be7f75f3475bdd752894153dfbf
mosquiiito0 <mosquiiito@naver.com>
2025-05-20 16:46:56 +0900

    New one

---
+++ Assets/02.Scripts/StudyLog.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class StudyLog : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Debug.Log("Start 함수 실행");//일반적인로그
+        //Debug.LogWarning("Start 함수 실행");//경고로그
+        //Debug.LogError("Start 함수 실행");//에러로그
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+
+
